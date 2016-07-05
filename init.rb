e0a490aece369208a0b6071c79d672662b276f4d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-04 23:17:37 -0300

    Move EacBase::Patches::JournalPatch para EventsManager::Patches::JournalPatch. Refs #408.

--- init.rb
+++ init.rb
@@ -4,6 +4,7 @@ require 'redmine'
 
 require 'events_manager/patches/issue_patch'
 require 'events_manager/patches/issue_relation_patch'
+require 'events_manager/patches/journal_patch'
 
 Redmine::Plugin.register :events_manager do
   name 'Events Manager'
