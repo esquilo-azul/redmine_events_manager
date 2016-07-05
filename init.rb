127529e2f751287b64c91f13fa1879992b9b0a5c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-04 23:03:31 -0300

    Plugin "events_manager". Refs #408.

---
+++ init.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :events_manager do
+  name 'Events Manager'
+  author 'Eduardo Henrique Bogoni'
+  description 'Management for events'
+  version '0.1.0'
+end
