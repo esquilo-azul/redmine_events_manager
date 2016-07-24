7393eceed9d940d0a21a58dfd2c9cce1f6fda45e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-24 16:24:25 -0300

    Download do log de exceções em background. Refs #408.

--- app/controllers/event_exceptions_controller.rb
+++ app/controllers/event_exceptions_controller.rb
@@ -7,9 +7,21 @@ class EventExceptionsController < ApplicationController
     conf.actions.exclude :create, :update, :delete
     conf.list.columns = [:created_at, :event_entity, :event_action, :listener_class]
     conf.list.sorting = { created_at: :desc }
+    conf.action_links.add :download, type: :member, label: 'Download', page: true
+  end
+
+  def download
+    ex = find_if_allowed(params[:id], :read)
+    send_data ex.attributes.to_yaml, filename: download_filename(ex)
   end
 
   private
+
+  def download_filename(ex)
+    [Setting.host_name, 'exception', ex.id.to_s, ex.created_at.to_s].map do |s|
+      s.parameterize.strip
+    end.select(&:present?).join('_')
+  end
 
   def clear_event_exception_unchecked
     EventsManager::Settings.event_exception_unchecked = false if
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 RedmineApp::Application.routes.draw do
-  resources(:event_exceptions) { as_routes }
+  resources(:event_exceptions) do
+    as_routes
+    member do
+      get :download
+    end
+  end
 end
