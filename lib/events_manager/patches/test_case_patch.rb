0d0826bed6fa89a123c2ad3e688608ea2752870a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-08-28 17:57:37 -0300

    "test_helper.rb" de plugins como patches. Refs 409.

---
+++ lib/events_manager/patches/test_case_patch.rb
@@ -0,0 +1,19 @@
+module EventsManager
+  module Patches
+    module TestCasePatch
+      def self.included(base)
+        base.setup do
+          EventsManager.delay_disabled = true
+          EventsManager::Settings.event_exception_unchecked = false
+        end
+      end
+    end
+  end
+end
+
+if Rails.env.test?
+  require Rails.root.join('test', 'test_helper.rb')
+  unless ::ActiveSupport::TestCase.included_modules.include? EventsManager::Patches::TestCasePatch
+    ::ActiveSupport::TestCase.send(:include, EventsManager::Patches::TestCasePatch)
+  end
+end
--- test/models/event_exception_test.rb
+++ test/models/event_exception_test.rb
@@ -1,4 +1,4 @@
-require_relative '../test_helper'
+require 'test_helper'
 
 class DummyListener
   def initialize(event)
