ad2d3eb71720408f2601d74407dab0c6c77c8855
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-01-23 22:58:07 -0200

    Recupera recebimento de repositório Git. Refs #276.

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ require 'events_manager/patches/issue_relation_patch'
 require 'events_manager/patches/journal_patch'
 require 'events_manager/patches/test_case_patch'
 require 'events_manager/patches/time_entry_patch'
-require 'events_manager/patches/hooks/redmine_patch'
+require 'events_manager/patches/repository/git_patch'
 require 'events_manager'
 require_dependency 'events_manager/hooks/add_assets'
 
