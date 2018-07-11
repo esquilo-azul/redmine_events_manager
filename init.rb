66187834d74939d29e9245dde8e8f246a4c1bd85
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-07-11 14:31:46 -0300

    Bump to '0.4.0'.

--- init.rb
+++ init.rb
@@ -15,7 +15,7 @@ Redmine::Plugin.register :redmine_events_manager do
   name 'Events Manager'
   author 'Eduardo Henrique Bogoni'
   description 'Management for events'
-  version '0.3.0'
+  version '0.4.0'
 
   settings default: { event_exception_unchecked: false }
 
