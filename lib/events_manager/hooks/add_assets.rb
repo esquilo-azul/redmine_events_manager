c95dc22f98f5f75ca040a3424962c1eff760f026
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-04-12 00:08:55 -0300

    Fix plugin name.

--- lib/events_manager/hooks/add_assets.rb
+++ lib/events_manager/hooks/add_assets.rb
@@ -3,7 +3,7 @@ module EventsManager
     class AddAssets < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(_context = {})
         header = ''
-        header << stylesheet_link_tag(:application, plugin: 'events_manager') + "\n"
+        header << stylesheet_link_tag(:application, plugin: 'redmine_events_manager') + "\n"
         header
       end
     end
