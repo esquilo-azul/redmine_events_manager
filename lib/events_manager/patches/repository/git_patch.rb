dda992a7dee124d89390e6e43ed7c38f5633731c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-24 15:33:05 -0300

    EventsManager::Patches::Repository::GitPatch: replace "alias_chain_method" with "prepend".

--- lib/events_manager/patches/repository/git_patch.rb
+++ lib/events_manager/patches/repository/git_patch.rb
@@ -1,20 +1,12 @@
+# frozen_string_literal: true
+
 module EventsManager
   module Patches
     module Repository
       module GitPatch
-        def self.included(base)
-          base.send(:include, InstanceMethods)
-
-          base.class_eval do
-            alias_method_chain :fetch_changesets, :trigger_event
-          end
-        end
-
-        module InstanceMethods
-          def fetch_changesets_with_trigger_event
-            fetch_changesets_without_trigger_event
-            EventsManager.trigger(::Repository, :receive, self)
-          end
+        def fetch_changesets
+          super
+          EventsManager.trigger(::Repository, :receive, self)
         end
       end
     end
@@ -22,5 +14,5 @@ module EventsManager
 end
 
 unless ::Repository::Git.included_modules.include?(EventsManager::Patches::Repository::GitPatch)
-  ::Repository::Git.include(EventsManager::Patches::Repository::GitPatch)
+  ::Repository::Git.prepend(EventsManager::Patches::Repository::GitPatch)
 end
