68b8c7f060e2fdf44335568f43bb0b4686bbac16
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-07 13:26:21 -0300

    RedmineEventsManager::TestConfig: extract from EventsManager::Patches::TestCasePatch.

--- lib/events_manager/patches/test_case_patch.rb
+++ lib/events_manager/patches/test_case_patch.rb
@@ -4,11 +4,7 @@ module EventsManager
   module Patches
     module TestCasePatch
       def self.included(base)
-        base.setup do
-          EventsManager.delay_disabled = true
-          EventsManager.log_exceptions_disabled = true
-          EventsManager::Settings.event_exception_unchecked = false
-        end
+        base.setup { ::RedmineEventsManager::TestConfig.new.before_each }
       end
     end
   end
---
+++ lib/redmine_events_manager/test_config.rb
@@ -0,0 +1,9 @@
+module RedmineEventsManager
+  class TestConfig
+    def before_each
+      EventsManager.delay_disabled = true
+      EventsManager.log_exceptions_disabled = true
+      EventsManager::Settings.event_exception_unchecked = false
+    end
+  end
+end
