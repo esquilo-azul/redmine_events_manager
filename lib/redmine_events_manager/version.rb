5a62473f141316749421032094db6f728c9e7337
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-11 11:01:39 -0300

    Bump to '0.4.15'.

--- lib/redmine_events_manager/version.rb
+++ lib/redmine_events_manager/version.rb
@@ -4,7 +4,7 @@ module RedmineEventsManager
   AUTHOR = 'Eduardo Henrique Bogoni'
   HOMEPAGE = 'https://github.com/esquilo-azul/redmine_events_manager'
   SUMMARY = 'Management for events'
-  VERSION = '0.4.14'
+  VERSION = '0.4.15'
 
   module Version
   end
